2429758121abd161d9aeaabef74d90454fde7ad6
dlavrent <dlvrntvch@gmail.com>
2021-12-15 17:23:56 -0500

    remove unnecessary files

--- SET_DIRECTORY_PATH.m
+++
@@ -1,3 +0,0 @@
-% set your local machine's path to this directory, to be used in all MATLAB scripts
-analysis_dir_path = 'C:\Users\dB\deBivort\projects\OLOI_analysis\';
-save('analysis_dir_path')
--- blue_yellow_colormap/blue_yellow_colormap.m
+++
@@ -1,3 +0,0 @@
-cruzbay=interp1([1 102 153 256],[0 0 1; 0 .6 .8; 0 .95 0; 1 1 0],1:256);
-
-imagesc(1:256);colormap(cruzbay)
