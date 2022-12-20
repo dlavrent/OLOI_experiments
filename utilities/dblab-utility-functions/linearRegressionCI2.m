b89e3900a81d44177351119b010e5ff4bb00cec2
dlavrent <dlvrntvch@gmail.com>
2022-12-20 17:06:30 -0500

    replace linearRegressionCI with linearRegressionCI2, saving xVals

--- utilities/dblab-utility-functions/linearRegressionCI2.m
+++ utilities/dblab-utility-functions/linearRegressionCI2.m
@@ -1,19 +1,19 @@
-function out=linearRegressionCI(X,Y)
+function out=linearRegressionCI2(X,Y,fitOrder,figureBool,xMin,xMax)
 
 bsReps=2000;
 xPoints=100;
-fitOrder=3;
+%fitOrder=3;
 
-figureBool=1;
+%figureBool=1;
 lineColor=[0 0 0];
 ciColor=[0.8 0.8 0.8];
 
 linFits=zeros(bsReps,xPoints);
 nPoints=length(X);
-xMin=min(X);
-xMax=max(X);
-xVals=linspace(xMin-(xMax-xMin)*0.05,xMax+(xMax-xMin)*0.05,xPoints);
-
+%xMin=min(X);
+%xMax=max(X);
+%xVals=linspace(xMin-(xMax-xMin)*0.05,xMax+(xMax-xMin)*0.05,xPoints);
+xVals=linspace(xMin,xMax,xPoints);
 
 h=waitbar(0,'bootstrapping progress');
 for i=1:bsReps
@@ -34,14 +34,14 @@ close(h);
 
 out.fits=linFits;
 out.pOverall=polyfit(X,Y,fitOrder);
-
+out.xVals=xVals;
 
 if figureBool==1
    
     figure;
     hold on;
     
-    areaBar(xVals,polyval(out.pOverall,xVals),2*std(out.fits),lineColor,ciColor);
+    areaBar(out.xVals,polyval(out.pOverall,out.xVals),2*std(out.fits),lineColor,ciColor);
     scatter(X,Y,320,'k.');
 end
 
